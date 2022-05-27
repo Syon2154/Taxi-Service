e0273b9d31a6448f06f146044de28a1591678201
Andrii Synelnyk <sinelnik2154@gmail.com>
2022-05-27 17:11:14 +0300

    Taxi service was created

---
+++ src/main/java/taxi/controller/driver/DeleteDriverController.java
@@ -0,0 +1,25 @@
+package taxi.controller.driver;
+
+import java.io.IOException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import taxi.lib.Injector;
+import taxi.service.DriverService;
+
+public class DeleteDriverController extends HttpServlet {
+    private static final Logger logger = LoggerFactory.getLogger(DeleteDriverController.class);
+    private static final Injector injector = Injector.getInstance("taxi");
+    private final DriverService driverService = (DriverService) injector
+            .getInstance(DriverService.class);
+
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        Long id = Long.parseLong(req.getParameter("id"));
+        driverService.delete(id);
+        logger.info("Driver was deleted. Params: id = {}", id);
+        resp.sendRedirect(req.getContextPath() + "/drivers");
+    }
+}
